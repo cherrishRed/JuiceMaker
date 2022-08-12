f42f1c45748dfee14cc2bd2b3d442d4accb08028
Red <seryprk@gmail.com>
2022-08-13 05:34:50 +0900

    ✨FEAT : 프로젝트 생성
    Signed-off-by: Red <seryprk@gmail.com>

---
+++ JuiceMaker/JuiceMaker/JuiceMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuiceMakerApp.swift
+//  JuiceMaker
+//
+//  Created by Red on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct JuiceMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
