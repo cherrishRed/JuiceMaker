b0ac08f8e80241e92a749d2b4c6587f131a7ca8f
Red <seryprk@gmail.com>
2022-08-27 02:27:26 +0900

    🚙MOVE : IngredientView 파일 분리
    Signed-off-by: Red <seryprk@gmail.com>

--- JuiceMaker/JuiceMaker/CustomJuice/CustomJuiceView.swift
+++ JuiceMaker/JuiceMaker/CustomJuice/CustomJuiceView.swift
@@ -44,49 +44,7 @@ struct CustomJuiceView: View {
     }
 }
 
-struct IngredientView: View {
-  private let fruits: [Fruit] = Fruit.allCases
-  @StateObject var vm: CustomJuiceCellViewModel
-  
-  var body: some View {
-    ZStack {
-      RoundedRectangle(cornerRadius: 15)
-        .fill(.white)
-        .frame(width: 250, height: 50, alignment: .center)
-      
-      HStack(spacing: 20) {
-        Menu {
-          Picker(selection: $vm.fruit) {
-            ForEach(fruits, id: \.rawValue) { value in
-              Text(value.icon)
-                .tag(value)
-            }
-          } label: {}
-        } label: {
-          Text(vm.fruit.icon)
-            .font(Font.custom("TossFaceFontMac", size: 30))
-        }.id(vm.fruit)
-        
-        Text("X")
-          .font(Font.custom("BMJUAOTF", size: 30))
-        
-        Menu {
-          Picker(selection: $vm.amount) {
-            ForEach(1..<21, id: \.self) { value in
-              Text("\(value)")
-                .tag(value)
-            }
-          } label: {}
-        } label: {
-          Text("\(vm.amount)")
-            .font(Font.custom("BMJUAOTF", size: 30))
-            .foregroundColor(.black)
-        }.id(vm.amount)
-      }
-      
-      }
-  }
-}
+
 
 struct CustomJuiceView_Previews: PreviewProvider {
     static var previews: some View {
